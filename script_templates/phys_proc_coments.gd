de73393e0f5be3ae02b6d97664933f5daa585e13
kodlon <45404949+kodlon@users.noreply.github.com>
2020-12-25 11:01:51 +0200

    Godot

---
+++ script_templates/phys_proc_coments.gd
@@ -0,0 +1,10 @@
+#-------------------------------#
+#           %BASE% v.1          #
+#-------------------------------#
+
+
+extends %BASE%
+
+
+func _physics_process(delta)%VOID_RETURN%:
+%TS%pass
